95fde863fda79b29bf5355c6e68a847ddc053b25
Cadu Guerra <cadu.guerra@gmail.com>
2023-12-13 20:02:38 -0300

    Finishing client.go

--- client/client.go
+++ client/client.go
@@ -1,5 +1,54 @@
 package main
 
-func main() {
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
 
+type Cotation struct {
+	Usdbrl Usdbrl `json:"USDBRL"`
+}
+
+type Usdbrl struct {
+	Bid string `json:"bid"`
+}
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
+	defer cancel()
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080/cotacao", nil)
+	if err != nil {
+		panic(err)
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Erro ao ler a resposta: %v\n", err)
+	}
+	var cotation Cotation
+
+	err = json.Unmarshal(body, &cotation)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Erro ao fazer parse da resposta: %v\n", err)
+	}
+
+	file, err := os.Create("cotacao.txt")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Erro ao Criar o arquivo: %v\n", err)
+	}
+	defer file.Close()
+	_, err = file.WriteString(fmt.Sprintf("Dólar: %s", cotation.Usdbrl.Bid))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Erro ao escrever no arquivo: %v\n", err)
+	}
+
+	fmt.Println("Arquivo criado com sucesso!")
 }
