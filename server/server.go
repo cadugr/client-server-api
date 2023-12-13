384cac1db9878a9c82f0e13b664f0076d7a09e84
Cadu Guerra <cadu.guerra@gmail.com>
2023-12-13 19:32:38 -0300

    Finishing server.go

--- server/server.go
+++ server/server.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
-	"fmt"
 	"io"
 	"net/http"
-	"os"
+	"time"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
-type Cotation struct {
-	Usdbrl Usdbrl `json:"USDBRL"`
+const database string = "client-server-api.db"
+
+type Cambio struct {
+	Cotation Cotation `json:"USDBRL"`
 }
 
-type Usdbrl struct {
+type Cotation struct {
+	ID         int    `gorm:"primarykey" json:"-"`
 	Code       string `json:"code"`
 	Codein     string `json:"codein"`
 	Name       string `json:"name"`
@@ -32,30 +38,72 @@ func main() {
 }
 
 func FindCotationHandler(w http.ResponseWriter, r *http.Request) {
-	cotation, err := FindCotation()
+	cambio, err := FindCotation()
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Erro ao buscar as cotações."))
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(cotation)
+	json.NewEncoder(w).Encode(cambio)
 }
 
-func FindCotation() (*Cotation, error) {
-	req, err := http.Get("https://economia.awesomeapi.com.br/json/last/USD-BRL")
+func FindCotation() (*Cambio, error) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
+	ctxDatabase, cancelDatabase := context.WithTimeout(context.Background(), 10*time.Millisecond)
+	req, err := http.NewRequestWithContext(ctx, "GET", "https://economia.awesomeapi.com.br/json/last/USD-BRL", nil)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Erro ao fazer a requisição: %v\n", err)
+		panic(err)
 	}
-	defer req.Body.Close()
-	body, err := io.ReadAll(req.Body)
+	defer cancel()
+	defer cancelDatabase()
+	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Erro ao ler a resposta: %v\n", err)
+		panic(err)
 	}
-	var cotation Cotation
-	err = json.Unmarshal(body, &cotation)
+	body, err := io.ReadAll(res.Body)
+	defer res.Body.Close()
+	if err != nil {
+		panic(err)
+	}
+	var cambio Cambio
+	err = json.Unmarshal(body, &cambio)
 	if err != nil {
 		return nil, err
 	}
-	return &cotation, nil
+
+	InsertCotation(ctxDatabase, cambio)
+
+	return &cambio, nil
+}
+
+func InsertCotation(context context.Context, cambio Cambio) {
+	db, err := CriaConexao()
+	if err != nil {
+		panic(err)
+	}
+	db.Create(&Cotation{
+		Code:       cambio.Cotation.Code,
+		Codein:     cambio.Cotation.Codein,
+		Name:       cambio.Cotation.Name,
+		High:       cambio.Cotation.High,
+		Low:        cambio.Cotation.Low,
+		VarBid:     cambio.Cotation.VarBid,
+		PctChange:  cambio.Cotation.PctChange,
+		Bid:        cambio.Cotation.Bid,
+		Ask:        cambio.Cotation.Ask,
+		Timestamp:  cambio.Cotation.Timestamp,
+		CreateDate: cambio.Cotation.CreateDate,
+	}).WithContext(context)
+}
+
+func CriaConexao() (*gorm.DB, error) {
+	db, err := gorm.Open(sqlite.Open(database), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	db.AutoMigrate(&Cotation{})
+	return db, nil
 }
